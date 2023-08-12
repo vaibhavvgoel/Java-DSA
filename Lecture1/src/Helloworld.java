49773c6066b3fb7a7823e4bece7f2a4ef3e35799
Vaibhav Goel <139869900+vaibhavvgoel@users.noreply.github.com>
2023-08-12 13:47:29 +0530

    Lecture 1

---
+++ Lecture1/src/Helloworld.java
@@ -0,0 +1,10 @@
+
+public class Helloworld {
+	
+	
+	public static void main(String args[]) {
+		System.out.println("Helloworld!!!");
+		boolean arr[] = new boolean[5];
+		System.out.println(arr[0]);
+	}
+}
